594f6b79670c50b499243cf81357a9a51333689b
BAUMSA <158750611+BAUMSA@users.noreply.github.com>
2025-06-01 12:53:36 +0200

    Add files via upload

---
+++ myfolio/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#007BFF",
+        secondary: "#00ff88",
+        accent: "#00ffea",
+        dark: "#0a192f",
+      },
+    },
+  },
+  plugins: [],
+};
